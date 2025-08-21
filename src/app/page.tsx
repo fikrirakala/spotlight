7e001b222b0bd6bbe6f5ffffa5d640157846a301
Fikri Rakala <fikrirakala@gmail.com>
2025-08-22 07:34:22 +0800

    Create Hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,16 @@
 import { type Metadata } from "next";
 
+import Hero from "@/components/home/hero";
+
 export const metadata: Metadata = {
   description:
     "We are a development studio working at the intersection of design and technology.",
 };
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
---
+++ src/components/home/hero.tsx
@@ -0,0 +1,23 @@
+export default function Hero() {
+  return (
+    <div className="pt-32 sm:px-8">
+      <div className="mx-auto w-full max-w-7xl lg:px-8">
+        <div className="relative px-4 sm:px-8 lg:px-12">
+          <div className="mx-auto max-w-2xl lg:max-w-5xl">
+            <div className="max-w-2xl">
+              <h1 className="text-4xl font-bold tracking-tight sm:text-5xl">
+                Software designer, founder, and amateur astronaut.
+              </h1>
+              <p className="text-muted-foreground mt-6 text-base">
+                I’m Spencer, a software designer and entrepreneur based in New
+                York City. I’m the founder and CEO of Planetaria, where we
+                develop technologies that empower regular people to explore
+                space on their own terms.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
